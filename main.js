bdba399f8b3d4f21505fece19c97b3ca6db47d44
Eric Bichan <eric.bichan@gmail.com>
2015-11-16 22:40:32 -0500

    reformating, added readme

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 console.log("lets do it!");
+'use strict';
+
 var config = require('./config.js');
-var WunderlistSDK = require('wunderlist');
-var wunderlistAPI = new WunderlistSDK({
+var SDK = require('wunderlist');
+var API = new SDK({
   'accessToken': config.ACCESS_TOKEN,
   'clientID': config.CLIENT_ID
 });
 
-wunderlistAPI.http.lists.all()
-  .done(function (lists) {
-  	console.log(lists);
-    /* do stuff */
-  })
-  .fail(function () {
-    console.error('there was a problem');
-  });
+
+API.initialized.done(function () {
+  // Where handleListData and handleError are functions 
+  // 'http' here can be replaced with 'socket' to use a WebSocket connection for all requests 
+  API.http.lists.all()
+    .done(function (lists) {
+      handleLists(lists);
+      /* do stuff */
+    })
+    .fail(function () {
+      console.error('there was a problem');
+    });
+});
+
+function handleLists (lists) {
+  console.log(lists);
+}
